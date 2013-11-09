c3c610060f000b987caab088041e12750fa4e46e
TJH <tch399@gmail.com>
2013-11-09 11:37:07 -0500

    adds text to list in a new form

--- shoppingList.js
+++ shoppingList.js
@@ -1,5 +1,12 @@
 $(document).ready(function() {
     $('.add-item-button').click(function(){
-        $('.list-rows').append("<p>new</p>");
+    var userInput = $('.list-input').val();
+    console.log(userInput);
+    $('.list-rows').append("<div class=\"checkbox\"> " +
+            "<label> " +
+    "<input type=\"checkbox\" value=\"\"> " +
+    userInput +
+  "</label>" +
+"</div>");
     });
 });
