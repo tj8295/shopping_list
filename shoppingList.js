ed269bb076c708ebeeba8fbf4647bdc34e7e3e27
TJH <tch399@gmail.com>
2013-11-10 11:58:15 -0500

    Added div inside well

--- shoppingList.js
+++ shoppingList.js
@@ -1,18 +1,23 @@
 $(document).ready(function() {
+    //user clicks add item button
     $('.add-item-button').click(function(){
     var userInput = $('.list-input').val();
-    console.log(userInput);
     $('.list-rows').append("<div class=\"checkbox\"> " +
             "<label> " +
     "<input type=\"checkbox\" value=\"\"> " +
     userInput + " </input>" +
   "</label>" +
 "</div>");
-    });
+    }); //end add-item-button
 
+    //user clicks a checkbox
     $(document).on('click', '.checkbox', function(){
         $(this).toggleClass("completed-item");
         console.log("div clicked");
-
     });
+
+    //user clicks select all
+    $('.select-all-button').click(function(){
+        $('.checkbox').addClass('completed-item');
+    })
 });
